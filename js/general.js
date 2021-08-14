52bdb6b3c68eaa4a5c4fb85fbe838346e8a5653c
Fuser <vkapustynskyy24@gmail.com>
2021-08-14 10:00:27 +0300

    Added select-type quiz

--- js/general.js
+++ js/general.js
@@ -88,27 +88,153 @@ const BASE_OF_Task_With_Images = [
     }
 
 ];
-const IMG_BASE = [  '<img class="questionImg" src="src/1.jpg">',
-                    '<img class="questionImg" src="src/2.jpg">'];
+const BASE_OF_Task_Select = [
+    {
+        taskText: 'Official figures show that the number of people taking international flights is decreasing, ' +
+            'and that this is __${select#option}__ in significant changes to holidaying habits. ' +
+            'As the cost of air tickets increases, it appears that more and more families are choosing ' +
+            'to __${select#option}__ their summer holidays at home. People are also becoming more __${select#option}__ of the harm ' +
+            'that flying does to the environment, and see it as a way of helping to __${select#option}__ the planet, too. ' +
+            'For many parents a summer with no airport queues or overcrowded resorts may seem attractive, ' +
+            'but the idea might well be less __${select#option}__ with their teenage children, who are probably __${select#option}__ to flying off ' +
+            'to the Mediterranean or Miami as soon as school breaks up. So, the question is, how can ' +
+            'young people __${select#option}__ lots of fun when so much will be closed for the holidays, and so many of their ' +
+            'friends are __${select#option}__ to be away? The answer may lie at the local sports centre. Nowadays, many centres organise ' +
+            'summer activities aimed at young people __${select#option}__ either on indoor or outdoor sports. These might range, ' +
+            'for instance, from playing table tennis to __${select#option}__ mountain-biking. As well as being healthy ' +
+            'and enjoyable, taking part in activities like these is also an excellent way to __${select#option}__ new friends. ' +
+            'For the most popular activities, though, it is advisable to __${select#option}__ early for a place- perhaps two or ' +
+            'three months in advance.'
+    },
+    {
+        options: {
+            a: "leading",
+            b: "resulting",
+            c: "causing",
+            d: "creating"
+        },
+        correctAnswer: "b"
+    },
+    {
+        options: {
+            a: "pass",
+            b: "employ",
+            c: "use",
+            d: "spend"
+        },
+        correctAnswer: "d"
+    },
+    {
+        options: {
+            a: "aware",
+            b: "thoughtful",
+            c: "wise",
+            d: "familiar"
+        },
+        correctAnswer: "a"
+    },
+    {
+        options: {
+            a: "save",
+            b: "secure",
+            c: "guard",
+            d: "defend"
+        },
+        correctAnswer: "a"
+    },
+    {
+        options: {
+            a: "liked",
+            b: "popular",
+            c: "approved",
+            d: "accepted"
+        },
+        correctAnswer: "b"
+    },
+    {
+        options: {
+            a: "used",
+            b: "experienced",
+            c: "preferred",
+            d: "prepared"
+        },
+        correctAnswer: "a"
+    },
+    {
+        options: {
+            a: "do",
+            b: "live",
+            c: "cause",
+            d: "have"
+        },
+        correctAnswer: "d"
+    },
+    {
+        options: {
+            a: "positive",
+            b: "inevitable",
+            c: "bound",
+            d: "definite"
+        },
+        correctAnswer: "c"
+    },
+    {
+        options: {
+            a: "eager",
+            b: "keen",
+            c: "fond",
+            d: "enthusiastic"
+        },
+        correctAnswer: "b"
+    },
+    {
+        options: {
+            a: "going",
+            b: "cycling",
+            c: "playing",
+            d: "riding"
+        },
+        correctAnswer: "a"
+    },
+    {
+        options: {
+            a: "meet",
+            b: "know",
+            c: "join",
+            d: "make"
+        },
+        correctAnswer: "d"
+    },
+    {
+        options: {
+            a: "demand",
+            b: "apply",
+            c: "claim",
+            d: "order"
+        },
+        correctAnswer: "b"
+    }
+];
+
+const IMG_BASE = ['<img class="questionImg" src="src/1.jpg">',
+    '<img class="questionImg" src="src/2.jpg">'];
 
 function build(testBase, taskId) {
     let elementById = document.getElementById(taskId);
     let type = elementById.getAttribute("data-type");
-    if (type === "test"){
+    if (type === "test") {
         buildTest(testBase, taskId);
     }
-    if (type === "select-test"){
-        buildSelect();
+    if (type === "select") {
+        buildSelect(testBase, taskId);
     }
-    if (type === "input-test"){
-        buildInput();
+    if (type === "input") {
+        buildInput(testBase, taskId);
     }
 }
 
-window.onload = function (){
+window.onload = function () {
     build(BASE_OF_Task1, "task1");
     build(BASE_OF_Task_With_Images, "task2");
-
-    setEventListenerOnRadio();
-    markRadioAsCheckedIfIsExistInLocalStorage();
+    build(BASE_OF_Task_Select, "task3");
 }
--- js/quizBuilder.js
+++ js/quizBuilder.js
@@ -1,12 +1,11 @@
 var INPUTS_ID = 0;
-var TESTS_COUNTER = 0;
 var TASK_COUNTER = 0;
 var LESSON_NUMBER;
 const POPUP = document.getElementById("notAllChecked");
 const POPUPbg = document.getElementById("popup-background");
 const POPUPok = document.querySelector(".pure-dialog-buttons input[type='button'][value='Ок']");
 
-POPUPok.onclick = function (){
+POPUPok.onclick = function () {
     hidePopUp();
 }
 
@@ -15,7 +14,7 @@ function buildTest(baseOfTests, taskId) {
     TASK_COUNTER++;
     LESSON_NUMBER = document.getElementById("taskWrapper").getAttribute("lesson");
 
-    let isResultShown = showResultsIfStored(baseOfTests, taskId);
+    let isResultShown = showResultsIfStored(taskId);
     if (isResultShown === true) {
         INPUTS_ID = document.querySelectorAll("#" + taskId + " input").length;
         return;
@@ -23,7 +22,7 @@ function buildTest(baseOfTests, taskId) {
 
     baseOfTests = shuffle(baseOfTests, taskId);
 
-    let htmlWrapper = document.getElementById(taskId);
+    let taskWrapper = document.getElementById(taskId);
 
     baseOfTests.forEach((question, questionNumber) => {
 
@@ -41,7 +40,7 @@ function buildTest(baseOfTests, taskId) {
             );
             INPUTS_ID++;
         }
-        if (IMG_BASE[question.imgIndex]){
+        if (IMG_BASE[question.imgIndex]) {
             genereted_test_item.push(
                 `<li class="questionContainer" style='z-index: ${questionNumber - 2000}'>
                         ${IMG_BASE[question.imgIndex]}
@@ -58,12 +57,13 @@ function buildTest(baseOfTests, taskId) {
             );
         }
 
-        htmlWrapper.insertAdjacentHTML('beforeend', genereted_test_item);
-        TESTS_COUNTER++;
+        taskWrapper.insertAdjacentHTML('beforeend', genereted_test_item);
     })
-    htmlWrapper.insertAdjacentHTML('beforeend', `  <br><div class="score" id="${taskId}-score"></div>
-                          <button class="button" id="${taskId}-submit" onclick="checkTest(this)">Перевірити</button>
-                          <button class="button" id="${taskId}-showErrors" style="display:none">Показати неправильні</button>`);
+    taskWrapper.insertAdjacentHTML('beforeend', `<br><div class="score" id="${taskId}-score"></div>
+                          <button class="button" id="${taskId}-submit" onclick="checkTest(this)">Перевірити</button>`);
+
+    setEventListenerOnRadio();
+    markRadioAsCheckedIfIsExistInLocalStorage();
 
 }
 
@@ -76,16 +76,20 @@ function setEventListenerOnRadio() {
     })
 }
 
-function markRadioAsCheckedIfIsExistInLocalStorage(){
+function updateRadioStateInLocalStorage(radio) {
+    localStorage.setItem("radio:" + radio.name, radio.id);
+}
+
+function markRadioAsCheckedIfIsExistInLocalStorage() {
     let taskWrapper = document.getElementById("taskWrapper");
     let questionCounter =
         taskWrapper.querySelectorAll(" div.task-wrapper ol.task-container li.questionContainer").length;
 
-    for (let taskNumber = 1; taskNumber <= TASK_COUNTER; taskNumber ++) {
+    for (let taskNumber = 1; taskNumber <= TASK_COUNTER; taskNumber++) {
         for (let questionNumber = 0; questionNumber < questionCounter; questionNumber++) {
             let item = localStorage.getItem("radio:lesson" + LESSON_NUMBER + "task" + taskNumber + "Question" + questionNumber);
 
-            if (item !== null){
+            if (item !== null) {
                 let radioById = document.getElementById(item.valueOf());
                 radioById.checked = true;
             }
@@ -94,24 +98,20 @@ function markRadioAsCheckedIfIsExistInLocalStorage(){
     }
 }
 
-function updateRadioStateInLocalStorage(radio) {
-    localStorage.setItem("radio:" + radio.name, radio.id);
-}
-
-function checkTest(buttonFromTaskContainer){
+function checkTest(buttonFromTaskContainer) {
 
     let calledButtonId = buttonFromTaskContainer.getAttribute("id");
-    let seperatorIndex = calledButtonId.indexOf("-");
+    let separatorIndex = calledButtonId.indexOf("-");
 
-    let taskToCheckId = calledButtonId.substr(0, seperatorIndex);
+    let taskToCheckId = calledButtonId.substr(0, separatorIndex);
 
     let taskToCheck = document.querySelector("div.task-wrapper #" + taskToCheckId);
     const USER_ANSWERS_INPUTS = taskToCheck.querySelectorAll("input:checked");
     let GRADE = 0;
     let GRADE_DISPLAY = taskToCheck.querySelector("div.score");
 
-    if (USER_ANSWERS_INPUTS.length !== taskToCheck.querySelectorAll("li.questionContainer").length){
-        showPopUpNotAllChecked(taskToCheck);
+    if (USER_ANSWERS_INPUTS.length !== taskToCheck.querySelectorAll("li.questionContainer").length) {
+        showPopUpNotAllChecked(taskToCheckId);
         return;
     }
 
@@ -129,7 +129,7 @@ function checkTest(buttonFromTaskContainer){
         USER_ANSWERS_INPUTS[i].parentElement.style.border = "solid #5d16a2 2px";
 
         let imageLabel = USER_ANSWERS_INPUTS[i].parentNode.querySelector("label");
-        if (baseOfTest[i].correctAnswer !== USER_ANSWERS_INPUTS[i].getAttribute("value")){
+        if (baseOfTest[i].correctAnswer !== USER_ANSWERS_INPUTS[i].getAttribute("value")) {
 
             imageLabel.style.fontWeight = "bold";
             imageLabel.innerHTML += " <img class=\"right-wrong-test-img\" src=\"https://bit.ly/2ApIim1\" alt='wrong'/>";
@@ -155,47 +155,182 @@ function checkTest(buttonFromTaskContainer){
     GRADE_DISPLAY.style.visibility = "visible";
     GRADE_DISPLAY.style.opacity = 100;
 
-    let saveBasesOfTests = [];
-    let saveResults = [];
 
-    for (let i = 1; i <= TASK_COUNTER; i++) {
-        let baseExist = localStorage.getItem("base_lesson" + LESSON_NUMBER + "task" + i)
-        let resultsExist = localStorage.getItem("lesson" + LESSON_NUMBER + "task" + i)
-        if (baseExist){
-            saveBasesOfTests[i] = baseExist;
-        }
-        if(resultsExist){
-            saveResults[i] = resultsExist;
-        }
-    }
+    clearLocalStorageButSaveBasesAndResults(taskToCheck);
 
-    localStorage.clear();
-    localStorage.setItem("lesson" + LESSON_NUMBER + taskToCheckId, taskToCheck.innerHTML);
-
-    for (let i = 1; i <= TASK_COUNTER; i++) {
-        if (saveBasesOfTests[i]){
-            localStorage.setItem("base_lesson" + LESSON_NUMBER + "task" + i, saveBasesOfTests[i]);
-        }
-        if (saveResults[i]){
-            localStorage.setItem("lesson" + LESSON_NUMBER + "task" + i, saveResults[i]);
-        }
-    }
+    // let saveBasesOfTests = [];
+    // let saveResults = [];
+    //
+    // for (let i = 1; i <= TASK_COUNTER; i++) {
+    //     let baseExist = localStorage.getItem("base_lesson" + LESSON_NUMBER + "task" + i)
+    //     let resultsExist = localStorage.getItem("lesson" + LESSON_NUMBER + "task" + i)
+    //     if (baseExist) {
+    //         saveBasesOfTests[i] = baseExist;
+    //     }
+    //     if (resultsExist) {
+    //         saveResults[i] = resultsExist;
+    //     }
+    // }
+    //
+    // localStorage.clear();
+    // localStorage.setItem("lesson" + LESSON_NUMBER + taskToCheckId, taskToCheck.innerHTML);
+    //
+    // for (let i = 1; i <= TASK_COUNTER; i++) {
+    //     if (saveBasesOfTests[i]) {
+    //         localStorage.setItem("base_lesson" + LESSON_NUMBER + "task" + i, saveBasesOfTests[i]);
+    //     }
+    //     if (saveResults[i]) {
+    //         localStorage.setItem("lesson" + LESSON_NUMBER + "task" + i, saveResults[i]);
+    //     }
+    // }
 
 }
 
 
 // Select
-function buildSelect(){
+function buildSelect(baseOfSelectTests, taskId) {
+    TASK_COUNTER++;
+    LESSON_NUMBER = document.getElementById("taskWrapper").getAttribute("lesson");
 
+    let isResultShown = showResultsIfStored(taskId);
+    if (isResultShown === true) {
+        return;
+    }
+
+    let taskInnerHtml = baseOfSelectTests[0].taskText;
+    let onlyOptions = baseOfSelectTests.slice(1);
+    let generatedSelect = [];
+
+    onlyOptions.forEach((select, selectNumber) => {
+        let options = [];
+
+        for (option in select.options) {
+            // TODO: options shuffling
+            options.push(
+                `<option value="${option}">${select.options[option]}</option>`
+            );
+        }
+
+        generatedSelect.push(
+            `<div class="select" name="lesson${LESSON_NUMBER + taskId}Question${selectNumber}">
+                <select>
+                    <option selected hidden value="0">Select answer:</option>
+                    ${options.join('')}
+                </select>
+            </div>`
+        );
+    });
+
+    for (let i = 0; i < generatedSelect.length; i++) {
+        taskInnerHtml = taskInnerHtml.replace("__${select#option}__", generatedSelect[i]);
+    }
+
+    let taskElement = document.getElementById(taskId);
+    taskElement.insertAdjacentHTML("beforeend",
+        `<h2 style="text-align: center;">${taskElement.getAttribute("title")}</h2>`);
+
+    taskElement.insertAdjacentHTML("beforeend", taskInnerHtml);
+
+    taskElement.insertAdjacentHTML('beforeend', `<br><div class="score" id="${taskId}-score"></div>
+                          <button class="button" id="${taskId}-submit" onclick="checkSelect(this)">Перевірити</button>`);
+
+    setEventListenerOnOption();
+    markSelectAsCheckedIfIsExistInLocalStorage();
+
+}
+
+function setEventListenerOnOption() {
+    let taskWrapper = document.getElementById("taskWrapper");
+    let selectInTaskWrapper = taskWrapper.querySelectorAll("select");
+
+    selectInTaskWrapper.forEach(select => {
+        select.addEventListener('change', () => updateSelectStateInLocalStorage(select));
+    })
+}
+
+function updateSelectStateInLocalStorage(select) {
+    localStorage.setItem("select:" + select.parentNode.getAttribute("name"), select.value);
+}
+
+function markSelectAsCheckedIfIsExistInLocalStorage() {
+    let taskWrapper = document.getElementById("taskWrapper");
+    let allSelectsParents = taskWrapper.querySelectorAll("ol.task-container div.select");
+
+    for (let i = 0; i < allSelectsParents.length; i++) {
+        let item = localStorage.getItem("select:" + allSelectsParents[i].getAttribute("name"));
+
+        if (item !== null) {
+            let checkedOption = allSelectsParents[i].querySelector("select option[value=" + item.valueOf() + "]");
+            checkedOption.selected = true;
+        }
+    }
+
+}
+
+function checkSelect(buttonFromTaskContainer) {
+    let calledButtonId = buttonFromTaskContainer.getAttribute("id");
+    let separatorIndex = calledButtonId.indexOf("-");
+    let baseOfTask = BASE_OF_Task_Select.slice(1);
+    let taskToCheckId = calledButtonId.substr(0, separatorIndex);
+    let taskToCheck = document.querySelector("div.task-wrapper #" + taskToCheckId);
+    let checkedOptions = taskToCheck.querySelectorAll("option:checked");
+    let USER_ANSWERS_INPUTS = [];
+    let GRADE = 0;
+    let GRADE_DISPLAY = taskToCheck.querySelector("div.score");
+
+
+    for (let i = 0; i < checkedOptions.length; i++) {
+        if (checkedOptions[i].value !== "0") {
+            USER_ANSWERS_INPUTS.push(checkedOptions[i]);
+        }
+    }
+
+    let allSelectsInTask = taskToCheck.querySelectorAll("select");
+
+    if (USER_ANSWERS_INPUTS.length !== allSelectsInTask.length) {
+        showPopUpNotAllChecked(taskToCheckId);
+        return;
+    }
+
+    buttonFromTaskContainer.remove();
+
+    for (let i = 0; i < checkedOptions.length; i++) {
+        let parentElement = checkedOptions[i].parentElement;
+        parentElement.disabled = true;
+
+        if (checkedOptions[i].value === baseOfTask[i].correctAnswer) {
+            parentElement.style.backgroundColor = "green";
+            parentElement.parentElement.insertAdjacentHTML("beforeend",
+                '<img class="right-wrong-select-img" src="https://bit.ly/3hnnVpX" alt="right">');
+            GRADE++;
+        } else {
+            parentElement.style.backgroundColor = "red";
+            parentElement.parentElement.insertAdjacentHTML("beforeend",
+                '<img class="right-wrong-select-img" src="https://bit.ly/2ApIim1" alt="right">');
+        }
+
+    }
+
+    GRADE_DISPLAY.innerHTML += "Результат: " + GRADE + "/" + USER_ANSWERS_INPUTS.length;
+    GRADE_DISPLAY.style.visibility = "visible";
+    GRADE_DISPLAY.style.opacity = 100;
+
+    clearLocalStorageButSaveBasesAndResults(taskToCheck);
+
+}
+
+
+// Option
+function buildInput(baseOfInputTests, taskId) {
 }
 
 // Other
 
-function showResultsIfStored(baseOfTests, taskId){
+function showResultsIfStored(taskId) {
     LESSON_NUMBER = document.getElementById("taskWrapper").getAttribute("lesson");
     let taskInnerHtml = localStorage.getItem("lesson" + LESSON_NUMBER + taskId);
 
-    if (taskInnerHtml === null){
+    if (taskInnerHtml === null) {
         return false;
     }
     document.getElementById(taskId).innerHTML = taskInnerHtml;
@@ -217,9 +352,9 @@ function shuffle(baseOfTests, taskId) {
     }
 }
 
-function showPopUpNotAllChecked(taskToCheck){
+function showPopUpNotAllChecked(taskToCheckId) {
 
-    POPUP.title = capitalizeFirstLetter(taskToCheck.id);
+    POPUP.title = capitalizeFirstLetter(taskToCheckId);
     POPUP.style.visibility = "visible";
     POPUP.style.opacity = 100;
 
@@ -228,7 +363,7 @@ function showPopUpNotAllChecked(taskToCheck){
 
 }
 
-function hidePopUp(){
+function hidePopUp() {
 
     POPUP.style.visibility = "hidden";
     POPUP.style.opacity = 0;
@@ -242,3 +377,35 @@ function capitalizeFirstLetter(str) {
     return capitalized;
 }
 
+function clearLocalStorageButSaveBasesAndResults(taskToCheck) {
+    let taskToCheckId = taskToCheck.id;
+    let saveBasesOfTests = [];
+    let saveResults = [];
+
+    for (let i = 1; i <= TASK_COUNTER; i++) {
+        let baseExist = localStorage.getItem("base_lesson" + LESSON_NUMBER + "task" + i)
+        let resultsExist = localStorage.getItem("lesson" + LESSON_NUMBER + "task" + i)
+        if (baseExist) {
+            saveBasesOfTests[i] = baseExist;
+        }
+        if (resultsExist) {
+            saveResults[i] = resultsExist;
+        }
+    }
+
+    localStorage.clear();
+    localStorage.setItem("lesson" + LESSON_NUMBER + taskToCheckId, taskToCheck.innerHTML);
+
+    for (let i = 1; i <= TASK_COUNTER; i++) {
+        if (saveBasesOfTests[i]) {
+            localStorage.setItem("base_lesson" + LESSON_NUMBER + "task" + i, saveBasesOfTests[i]);
+        }
+        if (saveResults[i]) {
+            localStorage.setItem("lesson" + LESSON_NUMBER + "task" + i, saveResults[i]);
+        }
+    }
+}
+
+$("select").on("change", function () {
+    $('option:selected', this).hide().siblings().show();
+});
